d2e09e99feb23c7d67f21a698e26aaf67452bb00
Coby Lerer <coby_lerer@hotmail.com>
2022-03-16 16:53:01 +1100

    fix picking URL value

--- dist/index.js
+++ dist/index.js
@@ -8331,7 +8331,7 @@ const options = {
     quick: core.getBooleanInput('quick'),
     updateAdvisor: core.getBooleanInput('update_advisor'),
     minCVSSForIssue: parseFloat(core.getInput('min-cvss-for-issue')) || 0,
-    url: '',
+    url: core.getInput('url'),
     github_token: core.getInput('github_token', { required: true }),
     createIssues: core.getBooleanInput('create-issues'),
     allowDirty: core.getBooleanInput('allow-dirty'),
--- src/action.ts
+++ src/action.ts
@@ -8,7 +8,7 @@ const options: Options = {
     quick: core.getBooleanInput('quick'),
     updateAdvisor: core.getBooleanInput('update_advisor'),
     minCVSSForIssue: parseFloat(core.getInput('min-cvss-for-issue')) || 0,
-    url: '',
+    url: core.getInput('url'),
     github_token: core.getInput('github_token',{required:true}),
     createIssues: core.getBooleanInput('create-issues'),
     allowDirty: core.getBooleanInput('allow-dirty'),

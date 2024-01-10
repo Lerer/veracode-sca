d8115ae3c401d3b32d9a8f4c1db78dad35dfbf2d
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2024-01-10 07:35:53 +0100

    change PR comment layout

--- src/srcclr.ts
+++ src/srcclr.ts
@@ -204,7 +204,7 @@ export async function runAction (options: Options)  {
                     let commentBody = '<br>![](https://www.veracode.com/sites/default/files/2022-04/logo_1.svg)<br>'
                     commentBody += "<pre>Veraocde SCA Scan finished with exit code "+code+"\n"
                     commentBody += '\n<details><summary>Veracode SCA Scan details</summary><p>\n'
-                    //commentBody += output.replace(/    /g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+                    commentBody += output //.replace(/    /g, '&nbsp;&nbsp;&nbsp;&nbsp;');
                     commentBody += '</p></details>\n</pre>'
 
 
